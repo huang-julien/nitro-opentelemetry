5a01cd30628183520cadef2edd7d6ccfd7c6713d
Julien Huang <julien.h.dev@gmail.com>
2024-10-18 23:54:09 +0200

    fix: try use alias

--- src/module.ts
+++ src/module.ts
@@ -2,13 +2,18 @@ import type { NitroModule } from 'nitropack'
 import type { Plugin } from "rollup"
 import { presets } from './imports'
 import { resolvePath } from 'mlly'
+import MagicString from 'magic-string'
 
 export default <NitroModule>{
     async setup(nitro) {
         nitro.hooks.hook('rollup:before', (nitro, rollupConfig) => {
             if (!rollupConfig.plugins) rollupConfig.plugins = []
             // @ts-ignore
-            rollupConfig.plugins.push(injectInitPlugin())
+            rollupConfig.plugins.push(injectInitPlugin()) 
+        })
+        
+        nitro.options.alias['#nitro-opentelemetry/init'] = await resolvePath('nitro-opentelemetry/runtime/init', {
+            extensions: ['.mjs', '.ts']
         })
 
         if (nitro.options.imports) {
@@ -22,11 +27,15 @@ function injectInitPlugin(): Plugin {
         name: 'inject-init-plugin',
         async transform(code, id) {
             const moduleInfo = this.getModuleInfo(id)
-            if (moduleInfo?.isEntry) {
-                return {
-                    code: [`import '${await resolvePath('nitro-opentelemetry/runtime/init', { extensions: ['.js', '.mjs', '.cjs'] })}'`, code].join('\n')
+             if (moduleInfo?.isEntry) {
+                const s = new MagicString(code)
+                s.prepend(`import '#nitro-opentelemetry/init';`)
+                  return {
+                    code: s.toString(),
+                    map: s.generateMap({ hires: true }),
+                    moduleSideEffects: 'no-treeshake'
                 }
-            }
+            } 
         },
     }
 }
