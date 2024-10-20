a9319ab15afc85008bebe41ea11e03aecc85ded4
Julien Huang <julien.h.dev@gmail.com>
2024-10-20 22:53:27 +0200

    fix: correctly mark config file as side-effect

--- src/module.ts
+++ src/module.ts
@@ -4,16 +4,40 @@ import { presets } from './imports'
 import { resolvePath } from 'mlly'
 import MagicString from 'magic-string'
 import { getPresetFile } from './presets'
+import { normalize } from "pathe"
 
 export default <NitroModule>{
     async setup(nitro) {
+        nitro.options.alias['#nitro-opentelemetry/init'] = await getPresetFile(nitro)
+
         nitro.hooks.hook('rollup:before', (nitro, rollupConfig) => {
-            if (!rollupConfig.plugins) rollupConfig.plugins = []
-            // @ts-ignore
-            rollupConfig.plugins.push(injectInitPlugin())
+            if (!rollupConfig.plugins) rollupConfig.plugins = [];
+            (rollupConfig.plugins as Plugin[]).push({
+                name: 'inject-init-plugin',
+                async transform(code, id) {
+                    // transform nitro entry file but there's probably a better way
+                    if (id.includes(String.raw`runtime\entries`)) {
+                        const s = new MagicString(code)
+                        s.prepend(`import '#nitro-opentelemetry/init';`)
+                        return {
+                            code: s.toString(),
+                            map: s.generateMap({ hires: true }),
+                            moduleSideEffects: true
+                        }
+                    } 
+                    //  @todo find another way :/
+                    if (normalize(id) === nitro.options.alias['#nitro-opentelemetry/init']) {
+                        const s = new MagicString(code)
+                        return {
+                            moduleSideEffects: true, 
+                            code: s.toString(),
+                            map: s.generateMap({ hires: true }),
+                        }
+                    }
+                },
+            })
         })
 
-        nitro.options.alias['#nitro-opentelemetry/init'] = await getPresetFile(nitro)
 
         if (nitro.options.imports) {
             nitro.options.imports.presets.push(presets)
@@ -22,23 +46,5 @@ export default <NitroModule>{
         nitro.options.plugins.push(await resolvePath('nitro-opentelemetry/runtime/plugin', {
             extensions: ['.mjs', '.ts']
         }))
-    }
-}
-
-function injectInitPlugin(): Plugin {
-    return {
-        name: 'inject-init-plugin',
-        async transform(code, id) {
-            if (id.includes(String.raw`runtime\entries`)) {
-                const s = new MagicString(code)
-                s.prepend(`import '#nitro-opentelemetry/init';`)
-                return {
-                    code: s.toString(),
-                    map: s.generateMap({ hires: true }),
-                    moduleSideEffects: true
-                }
-            }
-
-        },
     }
 }
