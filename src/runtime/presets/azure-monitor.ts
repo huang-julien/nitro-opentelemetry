8a026334cf030ef2be4c866240cf7210c43843e5
Julien Huang <julien.huang@leetchi.com>
2024-11-08 12:01:50 +0100

    fix(azure-monitor): remove default instrumentation key

--- src/runtime/presets/azure-monitor.ts
+++ src/runtime/presets/azure-monitor.ts
@@ -5,7 +5,7 @@ import { UndiciInstrumentation } from "@opentelemetry/instrumentation-undici";
 const options: AzureMonitorOpenTelemetryOptions = {
   azureMonitorExporterOptions: {
     connectionString:
-      process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"] || "InstrumentationKey=f629321d-170e-4630-85d3-09908821ab5a;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=e970c659-1858-4040-b255-aac514ea70e8",
+      process.env["APPLICATIONINSIGHTS_CONNECTION_STRING"],
   },
 }
 
