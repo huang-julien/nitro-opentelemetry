5d41ca4bf1929d46822a6cd6e293c60cc32d18e7
Julien Huang <julien.h.dev@gmail.com>
2024-12-15 00:00:06 +0100

    fix(baselime-cf-worker): cleanup baselime-cf-worker

--- src/runtime/presets/baselime-cf-worker.ts
+++ src/runtime/presets/baselime-cf-worker.ts
@@ -1,16 +1,12 @@
 import { instrument, ResolveConfigFn } from '@microlabs/otel-cf-workers'
 import handler from "#nitro-entry-file"
-import { DiagConsoleLogger, DiagLogLevel, diag } from '@opentelemetry/api'
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG)
+
 export interface Env {
 	BASELIME_API_KEY: string
     SERVICE_NAME: string
 }
 
-// const handler = useNitroApp().h3App.handler
-console.log('RUN')
 const config: ResolveConfigFn = (env: Env, _trigger) => {
-    console.log(env)
 	return {
 		exporter: {
 			url: 'https://otel.baselime.io/v1',
