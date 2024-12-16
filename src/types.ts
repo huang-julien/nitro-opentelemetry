bd22ffb03595bec10b3627e3cb2ef6a95c484081
Julien Huang <julien.h.dev@gmail.com>
2024-12-16 19:22:53 +0100

    chore: update comments

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,10 @@ import type { TraceConfig } from "@microlabs/otel-cf-workers"
 
 export interface CustomPreset {
     name: 'custom'
+    /**
+     * The path to the initializer file.
+     * This file will be imported in the entry file and need to initialize the OpenTelemetry SDK or one of its providers.
+     */
     filePath: string
 }
 
@@ -16,10 +20,15 @@ export interface AzureMonitorPreset {
 export interface BaselimeNodePreset {
     name: 'baselime-node'
 }
- 
+
+/**
+ * Baselime Cloudflare Worker preset
+ * This preset is a wrapper around the @microlabs/otel-cf-workers
+ */
 export interface BaselimeCfWorkerPreset {
     name: 'baselime-cf-worker'
     /**
+     * The options to pass to the @microlabs/otel-cf-workers
      * This only accept serializable objects
      */
     options: TraceConfig
