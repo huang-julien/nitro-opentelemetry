e82511b7ba72d2de3c4bed7f6d7cf5cf4671a320
Julien Huang <julien.h.dev@gmail.com>
2025-01-09 16:06:56 +0100

    fix: add missing instru

--- test/fixtures/basic/init.ts
+++ test/fixtures/basic/init.ts
@@ -2,14 +2,10 @@
 import * as opentelemetry from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { UndiciInstrumentation } from '@opentelemetry/instrumentation-undici';
  
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new OTLPTraceExporter({
-    // optional - default url is http://localhost:4318/v1/traces
-    url: '<your-otlp-endpoint>/v1/traces',
-    // optional - collection of custom headers to be sent with each request, empty by default
-    headers: {},
-  }), 
-  instrumentations: [getNodeAutoInstrumentations()],
+  traceExporter: new OTLPTraceExporter(), 
+  instrumentations: [getNodeAutoInstrumentations(), new UndiciInstrumentation()],
 });
 sdk.start();
