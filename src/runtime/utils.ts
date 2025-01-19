6e347896c16dc3cbcb9aebdbe16e551460ece403
Julien Huang <julien.h.dev@gmail.com>
2025-01-19 18:45:44 +0100

    docs: update jsdoc

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,7 +1,7 @@
 import { defineEventHandler } from "h3";
 
 /**
- * @deprecated since 0.7.1. Use `defineEventHandler` instead.
+ * @deprecated since 0.7.1. Context is automatically kept within handlers. You can use `defineEventHandler` instead.
  */
 export function defineTracedEventHandler(handler: ReturnType<typeof defineEventHandler>) { 
     return defineEventHandler(handler)
